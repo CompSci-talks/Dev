14ba4ef57f6e9f1b9fe6b62fc88413c4277de53c
Mohammad Essam <mohammad.essam@2b.com.eg>
2026-03-15 18:29:55 +0200

    firebase initialization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,14 @@ export const environment = {
     supabase: {
         url: 'https://kxxgeprqldabbjmbtajl.supabase.co',
         key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4eGdlcHJxbGRhYmJqbWJ0YWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNDM2NTIsImV4cCI6MjA4ODgxOTY1Mn0.R1l1IPduyUvb5BTD5OsbuoqD0DrBraE768_4MnAfNEc'
+    },
+    firebaseConfig: {
+        apiKey: "AIzaSyBq_V-h82VJQIlMSsf-MBqqKQAs5KsQ1iY",
+        authDomain: "compsci-b5aa2.firebaseapp.com",
+        projectId: "compsci-b5aa2",
+        storageBucket: "compsci-b5aa2.firebasestorage.app",
+        messagingSenderId: "967108755777",
+        appId: "1:967108755777:web:8107aa80e359ad220261c0",
+        measurementId: "G-MCM2GBZV4C"
     }
 };
