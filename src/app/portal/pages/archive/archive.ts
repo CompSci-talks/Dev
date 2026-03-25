526094a2f136f1a448a1f7960c02b89d3e443e2f
Mohammad Essam <mohammad.essam@2b.com.eg>
2026-03-25 17:11:20 +0200

    dynamic badge color

--- src/app/portal/pages/archive/archive.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-archive',
-  imports: [],
-  templateUrl: './archive.html',
-  styleUrl: './archive.scss',
-})
-export class Archive {}
